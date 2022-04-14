7ba8e7b7d16cc6abdbd4ae3c3797ef00a2812160
Stanislav Rakitov <prosperousrf@gmail.com>
2022-04-14 12:02:51 +0300

    PersonBuilder

---
+++ src/PersonBuilder.java
@@ -0,0 +1,43 @@
+/**
+ * @author Stanislav Rakitov
+ */
+public class PersonBuilder {
+  private String name;
+  private String surname;
+  private int age;
+  private String address;
+  private PersonBuilder instance;
+
+  public PersonBuilder() {
+    this.address = "";
+    this.age = -1;
+  }
+
+  public PersonBuilder setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public PersonBuilder setSurname(String surname) {
+    this.surname = surname;
+    return this;
+  }
+  public PersonBuilder setAge(int age) {
+    if (age < 0) {
+      throw new IllegalArgumentException();
+    }
+    this.age = age;
+    return this;
+  }
+  public PersonBuilder setAddress(String address) {
+    this.address = address;
+    return this;
+  }
+
+  public Person build() {
+    if (name == null && surname == null) {
+      throw new IllegalStateException();
+    }
+    return new Person(this.name, this.surname, this.age, this.address);
+  }
+}
